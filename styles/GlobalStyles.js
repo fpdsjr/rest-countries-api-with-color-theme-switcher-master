fbd063778a1a648d3c88773e53bc78dbd9a3a327
fpdsjr <flaviopds.jr@gmail.com>
2022-02-11 15:35:54 -0300

    set up colors and fonts

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -8,7 +8,16 @@ export const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
 }
 
+  
+:root {
+  --darkBlue: hsl(209, 23%, 22%)
+  --verDarkBlue:hsl(207,26%,17%)
+  --veryDarkBlue:hsl(200,15%,8%)
+  --darkGray:hsl(0,0%,52%)
+  --veryLightGray:hsl(0,0%,98%)
+  --white:hsl(0,0%,100%)
 
+  }
 
 
 `;
