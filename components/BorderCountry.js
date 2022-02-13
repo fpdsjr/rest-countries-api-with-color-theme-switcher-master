b9b96cd1eace0450eaa0aac25281ada14a67e636
fpdsjr <flaviopds.jr@gmail.com>
2022-02-13 12:56:53 -0300

    implement borders countries button

--- components/BorderCountry.js
+++ components/BorderCountry.js
@@ -1,11 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function BorderCountry({ borders }) {
+  const [boardersCountries, setBoardersCountries] = useState([]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchApiCountry();
+  }, []);
+
+  const fetchApiCountry = async () => {
+    const string = borders.join(",");
+    const boardersCountries = await fetch(
+      `https://restcountries.com/v2/alpha?codes=${string}`
+    );
+    const result = await boardersCountries.json();
+    const boardersCountriesNames = result.map((country) => country.name);
+    setBoardersCountries(boardersCountriesNames);
+  };
+
+  const buttonRender = () => {
+    return boardersCountries.map((country) => (
+      <button
+        key={country}
+        type="submit"
+        onClick={() => router.push(`/countries/${country}`)}
+      >
+        {country}
+      </button>
+    ));
+  };
+
   return (
     <>
       <p>
-        <span>Border Countries: </span>{" "}
-        {borders ? borders.map((b) => b).join(" ") : "none"}
+        <span>Border Countries:</span> {buttonRender()}
       </p>
     </>
   );
